7baa84b936363f6bc832ff1ac0209e88e20d4402
0b1can0b <0b1can0b21@gmail.com>
2022-10-19 00:27:14 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import './App.scss';
+import "./App.scss";
 
 const App = () => {
   return (
-    <div className="App">
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, sunt
-      minima? Aspernatur odio illo beatae blanditiis debitis eum quae eaque!
+    <div className="app">
+      <div className="text">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit, sunt
+        minima? Aspernatur odio illo beatae blanditiis debitis eum quae eaque!
+      </div>
+      <div className="button">button</div>
     </div>
   );
 };
