aad1e57f99a8ba8966a372392ec268ab422cd4f4
0b1can0b <0b1can0b21@gmail.com>
2022-11-13 00:48:54 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
+import { useState, createContext, useContext } from "react";
 import "./App.scss";
 
 console.clear();
 
+const popupIsOpen = createContext(null);
+
+const Header = () => {
+  const [, setOpen] = useContext(popupIsOpen);
+  return (
+    <div className="header">
+      <div>header</div>
+      <div>
+        <button onClick={() => setOpen((prev) => !prev)}>open popup</button>
+      </div>
+    </div>
+  );
+};
+const Body = () => {
+  const [open] = useContext(popupIsOpen);
+  return (
+    <div className="body">
+      <div>body</div>
+      {!open && <div>popup is not open</div>}
+      {open && <div>popup is open</div>}
+    </div>
+  );
+};
+
 const App = () => {
-  return <div className="app">App</div>;
+  return (
+    <div className="app">
+      <popupIsOpen.Provider value={useState(false)}>
+        <Header />
+        <hr style={{ width: "100%" }} />
+        <Body />
+      </popupIsOpen.Provider>
+    </div>
+  );
 };
 
 export default App;
